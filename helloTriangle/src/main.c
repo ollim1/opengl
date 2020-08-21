2d7c6a9c33ca57ef7ee8b2143a11990dc4a33af9
Olli Määttä <ollimaatta@gmail.com>
2020-08-21 15:02:00 +0300

    continue working on triangle

--- helloTriangle/src/main.c
+++ helloTriangle/src/main.c
@@ -6,6 +6,20 @@ void processInput(GLFWwindow *window);
 
 int main(int argc, char **argv)
 {
+    float vertices[] = {
+        -0.5f,  -0.5f,  0.0f,
+        0.5f,   -0.5f,  0.0f,
+        0.0f,   -0.5f,  0.0f,
+    };
+    unsigned int VBO;
+    const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+    unsigned int vertexShader;
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -20,6 +34,22 @@ int main(int argc, char **argv)
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwMakeContextCurrent(window);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    int success;
+    char log[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, log);
+        printf("error, shader compilation failed: %s\n", log);
+    }
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
