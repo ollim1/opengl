7b0fe78c3807263a8142869b7f94805c6d3269a5
Olli Määttä <ollimaatta@gmail.com>
2020-11-06 11:52:10 +0200

    multiple cubes

---
+++ cubes/main.c
@@ -0,0 +1,328 @@
+// Compile with -DGL_GLEXT_PROTOTYPES
+#include "include/cglm/cglm.h"
+#include "include/cglm/call.h"
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+// Compile with -DGL_GLEXT_PROTOTYPES
+#include <GL/glext.h>
+#define  STB_IMAGE_IMPLEMENTATION
+#include "include/stb_image.h"
+
+float texCoords[] = {
+    0.0f, 0.0f, // lower left
+    1.0f, 0.0f, // lower right
+    0.5f, 1.0f, // top center
+};
+
+char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec2 aTexCoord;\n"
+"out vec2 TexCoord;\n"
+"uniform mat4 model;\n"
+"uniform mat4 view;\n"
+"uniform mat4 projection;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
+"   TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+"}\n";
+
+char *fragShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec2 TexCoord;\n"
+"uniform sampler2D texture1;\n"
+"uniform sampler2D texture2;\n"
+"void main()\n"
+"{\n"
+"   FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1 - TexCoord.x, TexCoord.y)), 0.2);\n"
+"}\n";
+
+void printError(int code);
+
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    SDL_Window *w = SDL_CreateWindow("test", 0, 0, 640, 480, SDL_WINDOW_OPENGL);
+    SDL_GL_CreateContext(w);
+
+    glClearColor(0.25, 0., 0., 1.);
+    glEnable(GL_DEPTH_TEST);
+
+    // setting up a polygon on which to map the texture
+    float vertices[] = {
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+
+    vec3 cubePositions[] = {
+	{ 0.0f,  0.0f,  0.0f}, 
+	{ 2.0f,  5.0f, -15.0f}, 
+	{-1.5f, -2.2f, -2.5f},  
+	{-3.8f, -2.0f, -12.3f},  
+	{ 2.4f, -0.4f, -3.5f},  
+	{-1.7f,  3.0f, -7.5f},  
+	{ 1.3f, -2.0f, -2.5f},  
+	{ 1.5f,  2.0f, -2.5f}, 
+	{ 1.5f,  0.2f, -1.5f}, 
+	{-1.3f,  1.0f, -1.5f}  
+    };
+    // set up buffers to configure vertex attributes
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    // texture coordinate
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // compiling shaders
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        printf("shader compilation error: %s\n", infoLog);
+        return -1;
+    }
+
+    unsigned int fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragShader, 1, &fragShaderSource, NULL);
+    glCompileShader(fragShader);
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
+        printf("shader compilation error: %s\n", infoLog);
+        return -1;
+    }
+
+    // linking shaders
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("shader linking error: %s\n", infoLog);
+        return -1;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragShader);
+
+    // loading textures
+    unsigned int texture1, texture2;
+
+    // texture 1
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // texture wrapping
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("wall.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        puts("failed to load texture file");
+        return -1;
+    }
+    stbi_image_free(data);
+
+    // texture 2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // texture wrapping
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    // texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    stbi_set_flip_vertically_on_load(1);
+    data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        puts("failed to load texture file");
+        return -1;
+    }
+    stbi_image_free(data);
+
+    // loading a picture onto the texture
+    // setting samplers for shader
+    glUseProgram(shaderProgram);
+    glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
+    glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1);
+    unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+    unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+    unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+
+    SDL_Event e;
+    while(1) {
+        int error = glGetError();
+        if (error != GL_NO_ERROR) {
+            printError(error);
+        }
+        if (e.type == SDL_QUIT) {
+            break;
+        } else if (e.type == SDL_KEYDOWN) {
+            if (e.key.keysym.sym == SDLK_ESCAPE || e.key.keysym.sym == SDLK_q) {
+                break;
+            }
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // bind texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        // view matrix
+        mat4 view = {
+            {1, 0, 0, 0,},
+            {0, 1, 0, 0,},
+            {0, 0, 1, 0,},
+            {0, 0, 0, 1,},
+        };
+        glm_translate(view, (vec3){0, 0, -3});
+
+        // projection matrix
+        mat4 projection;
+        glm_perspective(glm_rad(45), 800.0f / 600.0f, 0.1f, 100.0f, projection);
+
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, (float *) view);
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, (float *) projection);
+
+        // render container
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        for (int i = 0; i < 10; i++) {
+            // model matrix
+            mat4 model = {
+                {1, 0, 0, 0,},
+                {0, 1, 0, 0,},
+                {0, 0, 1, 0,},
+                {0, 0, 0, 1,},
+            };
+            glm_translate(model, cubePositions[i]);
+            glm_rotate(model, glm_rad(-66), (vec3){1, 0, 0});
+            glm_rotate(model, glm_rad(-66), cubePositions[i]);
+	    float angle = 20 * i;
+            glm_rotate(model, SDL_GetTicks() / 1000.0 + glm_rad(angle), (vec3){0, 0, 1});
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, (float *) model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        SDL_GL_SwapWindow(w);
+        SDL_PollEvent(&e);
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    return 0;
+}
+
+void printError(int code)
+{
+    printf("gl error: ");
+    switch (code) {
+        case GL_INVALID_ENUM:
+            puts("invalid  enum");
+            break;
+        case GL_INVALID_VALUE:
+            puts("invalid value");
+            break;
+        case GL_INVALID_OPERATION:
+            puts("invalid operation");
+            break;
+        case GL_INVALID_FRAMEBUFFER_OPERATION:
+            puts("invalid framebuffer operation");
+            break;
+        case GL_OUT_OF_MEMORY:
+            puts("out of memory");
+            break;
+        case GL_STACK_UNDERFLOW:
+            puts("stack underflow");
+            break;
+        case GL_STACK_OVERFLOW:
+            puts("stack overflow");
+            break;
+        case 0:
+            puts("glGetError error");
+            break;
+    }
+}
